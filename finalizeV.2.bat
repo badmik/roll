354c9188aed3ce19d22e96b751a22f4e9f812cf8
Mike Bader <badmik@gmail.com>
2018-04-11 21:46:00 +0200

    finalize

---
+++ finalizeV.2.bat
@@ -0,0 +1,55 @@
+start "" "C:\Program Files\MIGARATION.exe"
+%SystemRoot%\explorer.exe
+start "" "C:\Program Files (x86)\Microsoft Office\Office16\WINWORD.EXE"
+start "" "C:\Program Files (x86)\Microsoft Office\Office16\EXCEL.EXE"
+start "" "C:\Program Files (x86)\Microsoft Office\Office16\PowerPNT.EXE"
+start "" "C:\Program Files (x86)\Microsoft Office\Office16\OUTLOOK.EXE"
+start "" "C:\Program Files (x86)\CCCCWZZZZI.exe"
+start "" "%ProgramFiles(x86)%\Internet Explorer\iexplore.exe" 
+start "" "%ProgramFiles(x86)%\Internet Explorer\iexplore.exe" 
+start "" "%ProgramFiles(x86)%\Internet Explorer\iexplore.exe" 
+
+
+
+
+
+
+
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" www.web.de
+echo "opening Chrome"
+::start "" "Systemsteuerung\Alle Systemsteuerungselemente\System"
+
+
+
+
+::printers
+
+::explorer shell:::{A8A91A66-3A7D-4424-8D24-04E180695C7A}
+control /name Microsoft.DevicesAndPrinter
+echo "printers started"
+
+
+
+::https://superuser.com/questions/874849/change-what-closing-the-lid-does-from-the-commandline
+
+
+
+::https://docs.microsoft.com/en-us/windows/uwp/launch-resume/launch-settings-app
+
+
+::ms-settings:printers
+echo "opening printers"
+
+::ms-settings:display
+echo "opening display"
+::ms-settings:powersleep
+echo "opening powersleep"
+::ms-settings:batterysaver
+echo "opening batterysaver"
+
+::ms-settings:batterysaver-usagedetails
+echo "opening batterysaver2"
+
+
+::ms-settings:windowsupdate
+echo "opening windowsupdate"
